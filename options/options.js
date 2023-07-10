b38b953b40de3f881e2c0690dab217136a806555
Rakibul <mriidb@gmail.com>
2023-07-10 09:31:23 +0600

    Control new tab page url: wip

--- options/options.js
+++ options/options.js
@@ -2,8 +2,9 @@
 const saveOptions = () => {
     const language = document.getElementById('language').value;
     const showArabic = document.getElementById('showArabicVerse').checked;
+    const newTabPage = document.getElementById('newTabPage').checked;
 
-    chrome.storage.sync.set({ language: language, showArabic: showArabic },
+    chrome.storage.sync.set({ language: language, showArabic: showArabic, newTabPage: newTabPage },
         () => {
             // Update status to let user know options were saved.
             const status = document.getElementById('status');
@@ -11,6 +12,8 @@ const saveOptions = () => {
             setTimeout(() => {
                 status.textContent = '';
             }, 750);
+
+            chrome.runtime.sendMessage({ 'action': 'optionUpdated' });
         }
     );
 };
@@ -19,10 +22,11 @@ const saveOptions = () => {
 // stored in chrome.storage.
 const restoreOptions = () => {
     chrome.storage.sync.get(
-        { language: 'bengali', showArabic: true },
+        { language: 'bengali', showArabic: true, newTabPage: false },
         (items) => {
             document.getElementById('language').value = items.language;
             document.getElementById('showArabicVerse').checked = items.showArabic;
+            document.getElementById('newTabPage').checked = items.newTabPage;
         }
     );
 };
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,13 @@
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onmessage.addEventListener((message, sender, sendResponse) => {
+    if (message.action === 'optionUpdated') {
+        chrome.storage.sync.get('newTabPage', (data) => {
+            const newTabPageUrl = data.newTabPage || 'page/page.html';
 
-});
+            chrome.browserAction.setOverride({
+                'page': newTabPageUrl
+            });
+
+            console.log('New tab page updated: ' + newTabPageUrl);
+        });
+    }
+})
